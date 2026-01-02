d947ef2f453432c2e28f4e154dbfa6678d95aeea
Yunseong <dev.yunseong@gmail.com>
2026-01-02 19:35:10 +0900

    Add method to QuestRepository for fetching quests by user ID and state; implement quest checking and reward giving in QuestService

--- src/main/java/com/wordonline/matching/quest/repository/QuestRepository.java
+++ src/main/java/com/wordonline/matching/quest/repository/QuestRepository.java
@@ -1,9 +1,19 @@
 package com.wordonline.matching.quest.repository;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
+import com.wordonline.matching.quest.domain.QuestState;
 import com.wordonline.matching.quest.entity.Quest;
+
+import reactor.core.publisher.Flux;
 
 public interface QuestRepository extends R2dbcRepository<Quest, Long> {
 
+    @Query("""
+    SELECT q FROM quests q
+    JOIN user_quests uq ON q.id = uq.quest_id
+    WHERE uq.user_id = :userId AND uq.state = :state
+""")
+    Flux<Quest> findAllByUserIdAndState(Long userId, QuestState state);
 }
--- src/main/java/com/wordonline/matching/quest/service/QuestService.java
+++ src/main/java/com/wordonline/matching/quest/service/QuestService.java
@@ -2,8 +2,24 @@ package com.wordonline.matching.quest.service;
 
 import org.springframework.stereotype.Service;
 
+import com.wordonline.matching.quest.domain.QuestState;
+import com.wordonline.matching.quest.repository.QuestRepository;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @Service
+@RequiredArgsConstructor
 public class QuestService {
 
+    private final QuestRepository questRepository;
+    private final QuestChecker questChecker;
+    private final QuestRewardGiver rewardGiver;
 
+    public Mono<Void> checkQuests(long userId) {
+        return questRepository.findAllByUserIdAndState(userId, QuestState.IN_PROGRESS)
+                .filterWhen(quest -> questChecker.check(userId, quest))
+                .doOnNext(quest -> rewardGiver.give(userId, quest))
+                .then();
+    }
 }
