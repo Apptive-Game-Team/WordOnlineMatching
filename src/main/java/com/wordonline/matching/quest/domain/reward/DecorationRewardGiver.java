a1023ee0a4fd73a148e31df85322fa895f5a193a
Yunseong <dev.yunseong@gmail.com>
2026-01-02 19:46:14 +0900

    Add method to QuestRepository for fetching quests by user ID and state; implement quest checking and reward giving in QuestService

---
+++ src/main/java/com/wordonline/matching/quest/domain/reward/DecorationRewardGiver.java
@@ -0,0 +1,28 @@
+package com.wordonline.matching.quest.domain.reward;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.wordonline.matching.decoration.entity.UserDecoration;
+import com.wordonline.matching.decoration.repository.UserDecorationRepository;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@Scope("prototype")
+@Component("demo_rg")
+@RequiredArgsConstructor
+public class DecorationRewardGiver implements RewardGiver {
+
+    private final UserDecorationRepository userDecorationRepository;
+
+    @ParamName("decoration_id")
+    private Long decoId;
+
+    @Override
+    public Mono<Void> give(long userId) {
+        var userDeco = new UserDecoration(null, userId, decoId, false);
+        return userDecorationRepository.save(userDeco)
+                .then();
+    }
+}
